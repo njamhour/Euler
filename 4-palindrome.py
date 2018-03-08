0b17b88b086c27e3e4ee77cf3cab935a109382b4
njamhour <njjamhour@hotmail.com>
2018-03-08 04:32:09 -0300

    Correçao 4 exercicio

--- 4-palindrome.py
+++ 4-palindrome.py
@@ -4,7 +4,7 @@
 a = 1
 b = 1
 aux = 0
-#array = [0,0]
+auxDois = 0
 while(a<=999 and b<=999):
 	d = a * b
 #	print(d)
@@ -15,53 +15,12 @@ while(a<=999 and b<=999):
 	palindromo = str(d)
 	if(palindromo == palindromo[::-1]):
 #		print(palindromo)
-		if(d > 900000):
-			print(d)
-#	else:
-#		print(last)
-	
-
-	#for c in range(a,99):
-	#d = a * b
-	#a += 1
-	#print(d)
-	#print("A:", a, " B:", b)
-	#if(a>b):
-	#b += 1
-	#print("A:", a, " B:", b)
-	#d = a * b
-	#print(d)
-#while(a<=99 and b>=1):
-#c = a * b
-#palindromo = str(c)
-#if(palindromo==palindromo[::-1]):
-#print palindromo
-#a += 1
-#b -= 1
-	
-
-
-	#if(a == b):
-	#a += 1
-	#else:
-	#b =-1
-	#c = a * b
-	#b +- 1
-	#print(c)
-	#palindromo = str(c)
-	#if(palindromo == palindromo[::-1]):
-	#print(palindromo)
-	#else:
-	#b += 1
-	#a += 1
-	#hile(b<=99):
-	#c = a * b
-	#print(a, b)
-	#a += 1
-	#b += 1
-	#print(c)
-	#palindromo = str(c)
-	##teste = palindromo[:::-1]
-	#if(palindromo == palindromo[::-1]):
-	#	print(palindromo)
-	
+		if(auxDois == 0):
+			auxDois = d
+			#print(array[0])
+		else:
+			aux = d
+			#print(array[1])
+		if(auxDois < aux):
+			auxDois = aux		
+print(auxDois)
